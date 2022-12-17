57f02fb5f9079957dd8a59b7ae49b87347aa512b
Sergey <isergeytarhov@gmail.com>
2022-12-17 19:10:46 +0300

    add fix task 3

--- Homework_1.py
+++ Homework_1.py
@@ -18,7 +18,7 @@ print("x", "y", "z")
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            if not (x or y or z) == (not(x)) and not (y) and not (z):
+            if not (x or y or z) == (not x) and (not y) and (not z):
                 print(f"{x} {y} {z} Вырожение истинно")
             else:
                 print(f"{x} {y} {z} Вырожение ложно")
